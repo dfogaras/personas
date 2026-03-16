171fbf43368fd96cfecbc06043b97832ed4207ac
Daniel Fogaras <daniel.fogaras@gmail.com>
2026-03-16 11:20:21 +0100

    noop

--- backend/main.py
+++ backend/main.py
@@ -33,6 +33,7 @@ def _parse_args():
 
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
 logger = logging.getLogger(__name__)
 settings = load_settings(_parse_args().config)
 ai_service = AIService(settings)
